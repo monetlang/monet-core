7d5f76b5f9b04124173e070041295229bc9711ad
Pan Chasinga <jo.chasinga@gmail.com>
2024-03-13 13:17:54 -0700

    Add compiler that compiles basic binop

--- src/compiler.rs
+++ src/compiler.rs
@@ -2,14 +2,15 @@ use std::collections::HashMap;
 
 use inkwell::context::Context;
 use inkwell::builder::Builder;
-use inkwell::values::{BasicValueEnum, FloatValue, PointerValue};
+use inkwell::module::Module;
+use inkwell::values::{BasicValueEnum, PointerValue};
 use inkwell::types::AsTypeRef;
 use crate::ast::Expr;
 
 pub struct Compiler<'a, 'ctx> {
   pub context: &'ctx Context,
   pub builder: &'a Builder<'ctx>,
-  // pub module: &'a Module<'ctx>,
+  pub module: &'a Module<'ctx>,
   // pub function: &'a Function,
   // variables: HashMap<String, PointerValue<'ctx>>,
   variables: HashMap<String, BasicValueEnum<'ctx>>,
@@ -17,28 +18,33 @@ pub struct Compiler<'a, 'ctx> {
 }
 
 impl<'a, 'ctx> Compiler<'a, 'ctx> {
-  // #[llvm_versions(4.0..=14.0)]
-  // pub fn build_load(&self, ptr: PointerValue<'ctx>, name: &str) -> BasicValueEnum<'ctx> {
-  //     self.builder.build_load(ptr, name).unwrap()
-  // }
-
-  // #[llvm_versions(15.0..=latest)]
-  // pub fn build_load(&self, ptr: PointerValue<'ctx>, name: &str) -> BasicValueEnum<'ctx> {
-  //     self.builder.build_load(self.context.f64_type(), ptr, name).unwrap()
-  // }
-
   pub fn build_load(&self, ptr: PointerValue<'ctx>, name: &str) -> BasicValueEnum<'ctx> {
     self.builder.build_load(self.context.f64_type(), ptr, name).unwrap()
-}
+  }
 
   pub(crate) fn compile_expr(&self, expr: &Expr) -> Result<BasicValueEnum<'ctx>, &'static String> {
-    match *expr {
-      // Expr::Number(n) => Ok(self.context.f64_type().const_float(n)),
-      Expr::Number(n) => Ok(BasicValueEnum::FloatValue(self.context.f64_type().const_float(n))),
+    match expr {
+      Expr::Number(n) => Ok(BasicValueEnum::FloatValue(self.context.f64_type().const_float(*n))),
       Expr::Variable(ref name) => match self.variables.get(name.as_str()) {
-        // Some(var) => Ok(self.build_load(*var, name)),
         Some(var) => Ok(*var),
         None => todo!("what!"),
+      },
+      Expr::BinOp { op, lhs, rhs } => {
+        let lhs = self.compile_expr(lhs.as_ref())?;
+        let rhs = self.compile_expr(rhs.as_ref())?;
+        let l = lhs.into_float_value();
+        let r = rhs.into_float_value();
+        let plusop = self.builder.build_float_add(l, r, "subtmp").unwrap();
+        let minusop = self.builder.build_float_sub(lhs.into_float_value(), rhs.into_float_value(), "subtmp").unwrap();
+        let multop = self.builder.build_float_mul(lhs.into_float_value(), rhs.into_float_value(), "multmp").unwrap();
+        let divop = self.builder.build_float_div(lhs.into_float_value(), rhs.into_float_value(), "divtmp").unwrap();
+        match op {
+          '+' => Ok(BasicValueEnum::FloatValue(plusop)),
+          '-' => Ok(BasicValueEnum::FloatValue(minusop)),
+          '*' => Ok(BasicValueEnum::FloatValue(multop)),
+          '/' => Ok(BasicValueEnum::FloatValue(divop)),
+          _ => todo!("compile_expr: {:?}", expr),
+        }
       },
       _ => todo!("compile_expr: {:?}", expr),
     }
@@ -112,27 +118,29 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 
 #[cfg(test)]
 mod tests {
-  use std::{collections::HashMap, result};
 
-  use crate::ast::Function;
+  use std::hash::Hash;
 
-  use super::*;
+use super::*;
   use combine::parser::token::Value;
   use inkwell::context::Context;
   use inkwell::builder::Builder;
   use inkwell::llvm_sys::LLVMValue;
   use inkwell::values::AsValueRef;
+  use crate::ast::Function;
 
   #[test]
   fn test_compile_number() {
-    let context = Context::create();
-    let builder = context.create_builder();
-    let module = context.create_module("tmp");
+    let context = &Context::create();
+    let builder = &context.create_builder();
+    let module = &context.create_module("tmp");
+    let variables = HashMap::new();
 
-    let compiler = Compiler {
-      context: &context,
-      builder: &builder,
-      variables: HashMap::new(),
+    let mut compiler = Compiler {
+      context,
+      builder,
+      module,
+      variables,
     };
 
     let expr = Expr::Number(1.0);
@@ -143,15 +151,18 @@ mod tests {
 
   #[test]
   fn test_compile_variable() {
-    let context = Context::create();
-    let builder = context.create_builder();
+    let context = &Context::create();
+    let builder = &context.create_builder();
+    let module = &context.create_module("tmp");
     let mut variables = HashMap::new();
     let f64_type = context.f64_type();
     let a = BasicValueEnum::FloatValue(f64_type.const_float(1.2));
     variables.insert("x".to_string(), a);
-    let compiler = Compiler {
-      context: &context,
-      builder: &builder,
+
+    let mut compiler = Compiler {
+      context,
+      builder,
+      module,
       variables,
     };
 
@@ -159,5 +170,36 @@ mod tests {
     let result = compiler.compile_expr(&expr).unwrap();
     let s = result.to_string();
     assert_eq!(s, "\"double 1.200000e+00\"".to_string());
+  }
+
+  #[test]
+  fn test_compile_binop() {
+    let context = &Context::create();
+    let builder = &context.create_builder();
+    let module = &context.create_module("my_module");
+    let void_type = context.void_type();
+    let fn_type = void_type.fn_type(&[], false);
+    let function1 = module.add_function("do_nothing", fn_type, None);
+    let basic_block = context.append_basic_block(function1, "entry");
+    builder.position_at_end(basic_block);
+
+    let variables = HashMap::new();
+
+    let compiler = Compiler {
+      context,
+      builder,
+      module,
+      variables,
+    };
+
+    let expr = Expr::BinOp {
+      op: '+',
+      lhs: Box::new(Expr::Number(1.0)),
+      rhs: Box::new(Expr::Number(3.0)),
+    };
+
+    let result = compiler.compile_expr(&expr).unwrap();
+    let s = result.to_string();
+    assert_eq!(s, "\"double 4.000000e+00\"".to_string());
   }
 }
